4a8c24a1dccd8bcc771f62661204317b18b55d2b
CaxQler <sdd106485@gmail.com>
2026-08-07 22:36:03 +0700

    Create keys-dumper.lua

---
+++ keys-dumper.lua
@@ -0,0 +1,41 @@
+-- ==========================================
+-- 🔑 XZILER HUB - RANDOM KEYS DATABASE
+-- ==========================================
+
+return {
+    ["oiUF0wj8-03iajifoAJF0wgk-99x2ZmA"] = true,
+    ["kL91mWzq-09aOskf902JFAks-11pKxMz"] = true,
+    ["xP01kLsm-88231jAosifJWOg-89aSxLk"] = true,
+    ["vM39qOsk-1029jAoskfJ028s-44mZpQw"] = true,
+    ["zA90qOls-77291AskfJWOa91-02kLpXm"] = true,
+
+    ["bN48vCxzk-9021jOaskFJ021-99mKzPq"] = true,
+    ["rE82Wqso-1102jAoskJF0192-88xLmZa"] = true,
+    ["pQ19xZks-33291OasKF0192a-77kLpM3"] = true,
+    ["mK90sDko-88120OaskJF0912-11xZaQw"] = true,
+    ["cV28mZpq-77120AskfJWOa10-22kLpXz"] = true,
+
+    ["aX90qOls-12901AskfJWOa91-99mKzPq"] = true,
+    ["wE12xMsk-88291AoskJF0192-33kLpZa"] = true,
+    ["tR90vCxzk-0021jOaskFJ012-77mKzPq"] = true,
+    ["yU81sDko-11029OaskJF0192-44xZaQw"] = true,
+    ["iO90qOls-33291AskfJWOa91-22kLpXm"] = true,
+
+    ["oP12mWzq-77291AoskfJ028s-88mZpQw"] = true,
+    ["pL90xZks-11029OasKF0192a-99kLpM3"] = true,
+    ["aS81vCxzk-8821jOaskFJ021-33mKzPq"] = true,
+    ["dF90sDko-99120OaskJF0912-77xZaQw"] = true,
+    ["gH28mZpq-00120AskfJWOa10-11kLpXz"] = true,
+
+    ["jK90qOls-88291AskfJWOa91-44mKzPq"] = true,
+    ["lZ12xMsk-77291AoskJF0192-99kLpZa"] = true,
+    ["xC90vCxzk-1121jOaskFJ012-33mKzPq"] = true,
+    ["vB81sDko-33029OaskJF0192-88xZaQw"] = true,
+    ["bN90qOls-99291AskfJWOa91-77kLpXm"] = true,
+
+    ["mM12mWzq-00291AoskfJ028s-11mZpQw"] = true,
+    ["Qw81xZks-88029OasKF0192a-33kLpM3"] = true,
+    ["Er90vCxzk-7721jOaskFJ021-99mKzPq"] = true,
+    ["Ty81sDko-11120OaskJF0912-44xZaQw"] = true,
+    ["Ui28mZpq-33120AskfJWOa10-88kLpXz"] = true
+}
